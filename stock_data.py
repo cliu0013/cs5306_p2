1b1f9e1abd714190e611f582b85471461b5ee847
Zhen Lu <zl569@cornell.edu>
2021-11-12 18:13:50 -0500

    Update stock_data.py

--- stock_data.py
+++ stock_data.py
@@ -1,18 +1,24 @@
 import pandas as pd
 
-stock_name = 's&p'
 
-# read the data from the csv file with stock_name and save to a df variable 
-df = pd.read_csv('{}.csv'.format(stock_name))
+def get_stock_data(stock_name):
 
-# keep only the date and open price columns
-df = df[['Date','Open']]	
+    # read the data from the csv file with stock_name and save to a df variable 
+    df = pd.read_csv('{}.csv'.format(stock_name))
+
+    # keep only the date and open price columns
+    df = df[['Date','Open']]	
 
 
-#sort df by date
-df = df.sort_values(by='Date')
+    #sort df by date
+    df = df.sort_values(by='Date')
 
-# change the date column to the datetime format
-df['Date'] = pd.to_datetime(df['Date'])
+    # change the date column to the datetime format
+    df['Date'] = pd.to_datetime(df['Date'])
 
-print(df.head())
+    # remove dollar sign of the open price column and convert to float, if it is a string
+    if(stock_name == 'GME' or stock_name == 'gme'):	
+        df['Open'] = df['Open'].str.replace('$','').astype(float)
+    return df
+
+get_stock_data('gme')
