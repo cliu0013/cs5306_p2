a49156fa4470b1f7599ac2e1ac88dc2d395d2396
Zhen Lu <zl569@cornell.edu>
2021-11-12 16:25:07 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
-import csv
 import pandas as pd
 
-import csv
+# with open('reddit_wsb.csv', newline='') as f:
+#     reader = csv.reader(f)
+#     data = list(reader)
+#     print(data[0])
 
-with open('reddit_wsb.csv', newline='') as f:
-    reader = csv.reader(f)
-    data = list(reader)
-    print(data[0])
+# read data from csv file and convert to list of lists  #
+data = pd.read_csv('reddit_wsb.csv')
+data = data.values.tolist()
 
 # ['timestamp', 'title', 'body', 'score']
 data = list(map(lambda x: [x[7], x[0], x[6], x[1]], data))
@@ -14,4 +15,4 @@ data = list(map(lambda x: [x[7], x[0], x[6], x[1]], data))
 # filter GME...
 GME_data = list(filter(lambda x: "GME" in x[1], data))
 
-# print(GME_data[:10])
+print(GME_data[:10])
