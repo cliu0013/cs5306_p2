271232f1cdae627fc671b83a24a7534b40206fe9
Zhen Lu <zl569@cornell.edu>
2021-11-12 19:22:56 -0500

    Create get_sentiments.py

---
+++ get_sentiments.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+def get_verdict(pos, neu, neg):
+    if pos + neg < neu / 2:
+        return 0
+    if pos > neg * 1.2:
+        return 1
+    elif pos < neg:
+        return -1
+    else:
+        return 0
+
+def get_sentiments(stock_name):
+    filename = ''
+    if stock_name == 's&p':
+        filename = 'reddit_wsb_sentiment_counts.csv'
+    else:
+        filename = 'reddit_wsb_sentiment_counts_gme.csv'
+    df = pd.read_csv(filename)
+    #convert df to list
+    df_list = df.values.tolist()
+    dic = {}
+    for date, sentiment, count in df_list:
+        # convert date to datetime
+        date = pd.to_datetime(date)
+        if date not in dic:
+            dic[date] = {}
+        dic[date][sentiment] = count
+    
+    # for each date in dic, call get_verdict
+    ans = {}
+    for date in dic:
+        pos = dic[date].get('pos', 0)
+        neu = dic[date].get('neu', 0)
+        neg = dic[date].get('neg', 0)
+        verdict = get_verdict(pos, neu, neg)
+        ans[date] = verdict
+    return ans
+
+# print(get_sentiments('s&p'))
